f2d1e997a9aeb4e6a14228b4342f23900c091081
MANUEL CRISTOBAL NEYRA S - SGIS-05 <MCRISTOBALN@HMPP.COM>
2024-09-12 12:38:25 -0500

    feat: Add image and level in tha wizard

--- resources/views/components/forms/wizard.blade.php
+++ resources/views/components/forms/wizard.blade.php
@@ -22,6 +22,7 @@
                                 <option value="0" selected>SELECCIONE</option>
                                 <option value="EV">ELITE VARONES</option>
                                 <option value="EM">ELITE MUJERES</option>
+                                <option value="SM">SUPER MASTER</option>
                                 <option value="MM">MASTER</option>
                                 <option value="JU">JUVENILES</option>
                                 <option value="MEN">MENORES</option>
@@ -41,28 +42,31 @@
                         <div class="col-md-6">
                                         <h6 class="d-flex justify-content-between mb-3 form-step__block"><span><b>CATEGORIAS</b></span><span><b>EDADES</b></span></h6>
                                 <ul class="form-step__list">
-                                        <li class="text-muted my-sm-1 my-md-3">
+                                        <li class="text-muted my-sm-1 my-md-2">
                                             <small class="text-muted d-flex justify-content-between"><span><b>» ELITE VARONES MUJERES</b></span><span><b>Mayores de 18</b></span></small>          
                                         </li>
-                                        <li class="text-muted my-sm-1 my-md-3">
+                                        <li class="text-muted my-sm-1 my-md-2">
+                                            <small class="text-muted d-flex justify-content-between"><span><b>» SUPER MASTER</b></span><span><b>49 a más</b></span></small>        
+                                        </li>
+                                        <li class="text-muted my-sm-1 my-md-2">
                                             <small class="text-muted d-flex justify-content-between"><span><b>» MASTER</b></span><span><b>40 a más</b></span></small>        
                                         </li>
-                                        <li class="text-muted my-sm-1 my-md-3">
+                                        <li class="text-muted my-sm-1 my-md-2">
                                             <small class="text-muted d-flex justify-content-between"><span><b>» JUVENILES</b></span><span><b>15 - 16 - 17 años</b></span></small>          
                                         </li>
-                                        <li class="text-muted my-sm-1 my-md-3">
+                                        <li class="text-muted my-sm-1 my-md-2">
                                             <small class="text-muted d-flex justify-content-between"><span><b>» MENORES</b></span><span><b>12 - 13 - 14 años</b></span></small>          
                                         </li>
-                                        <li class="text-muted my-sm-1 my-md-3">
+                                        <li class="text-muted my-sm-1 my-md-2">
                                             <small class="text-muted d-flex justify-content-between"><span><b>» JUNIOR</b></span><span><b>10 - 11 años</b></span></small>          
                                         </li>
-                                        <li class="text-muted my-sm-1 my-md-3">
+                                        <li class="text-muted my-sm-1 my-md-2">
                                             <small class="text-muted d-flex justify-content-between"><span><b>» INFANTIL</b></span><span><b>7 - 8 - 9 años</b></span></small>          
                                         </li>
-                                        <li class="text-muted my-sm-1 my-md-3">
+                                        <li class="text-muted my-sm-1 my-md-2">
                                             <small class="text-muted d-flex justify-content-between"><span><b>» HABILIDADES ESPECIALES</b></span><span><b>Sordo y Mudo</b></span></small>          
                                         </li>
-                                        <li class="text-muted my-sm-1 my-md-3">
+                                        <li class="text-muted my-sm-1 my-md-2">
                                             <small class="text-muted d-flex justify-content-between"><span><b>» HABILIDADES ESPECIALES</b></span><span><b>Visual</b></span></small>          
                                         </li>
                                 </ul>
