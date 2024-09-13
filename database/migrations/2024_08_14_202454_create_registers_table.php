4d09da58768d879611bcb57525ace65009a90390
MANUEL CRISTOBAL NEYRA S - SGIS-05 <MCRISTOBALN@HMPP.COM>
2024-09-13 15:08:12 -0500

    fix: Change type value of  document

--- database/migrations/2024_08_14_202454_create_registers_table.php
+++ database/migrations/2024_08_14_202454_create_registers_table.php
@@ -15,24 +15,24 @@ return new class extends Migration
             $table->char('number_doc',50);
             $table->string('name',255);
             $table->string('lastname',255);
-            $table->boolean('genders');
-            $table->dateTime('born', $precision = 3);
+            $table->char('genders',1);
+            $table->date('born');
             $table->string('addresd',255)->nullable();
             $table->string('phone',20)->nullable();
-            $table->dateTime('create_at',$precision = 3);
-            $table->dateTime('update_at',$precision = 3);
+            $table->string('number_ins',10);
+            $table->char('type_doc',1);
             $table->unsignedBigInteger('id_country');
             $table->unsignedBigInteger('id_region');
             $table->unsignedBigInteger('id_province');
             $table->unsignedBigInteger('id_district');
             $table->unsignedBigInteger('id_level');
-            $table->dateTime('created_at', $precision = 3);
-            $table->dateTime('updated_at', $precision = 3);
             $table->foreign('id_country')->references('id_country')->on('countries');
             $table->foreign('id_region')->references('id_region')->on('regions');
             $table->foreign('id_province')->references('id_province')->on('provinces');
             $table->foreign('id_district')->references('id_district')->on('districts');
             $table->foreign('id_level')->references('id_level')->on('levels');    
+            $table->dateTime('create_at',$precision = 3);
+            $table->dateTime('update_at',$precision = 3);
         });
     }
 
