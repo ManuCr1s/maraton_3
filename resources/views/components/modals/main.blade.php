2ffe351d50781267657bfd21f8ddb8ee06246483
MANUEL CRISTOBAL NEYRA S - SGIS-05 <MCRISTOBALN@HMPP.COM>
2024-09-18 16:57:48 -0500

    feat: Creating component modal and add page welcome

---
+++ resources/views/components/modals/main.blade.php
@@ -0,0 +1,27 @@
+<a type="button" class="button button--orange" data-toggle="modal" data-target="#staticBackdrop">
+    <i class="fa-solid fa-print icon"></i>VERIFICA TU INSCRIPCION 
+</a>
+<div class="modal fade" id="staticBackdrop" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+                <div class="modal-content">
+                        <div class="modal-header">
+                                <h5 class="modal-title" id="staticBackdropLabel">VERIFICA TU INSCRIPCION</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                                </button>
+                        </div>
+                        <form id="form_dni">
+                                <div class="modal-body">
+                                    <div class="form-group">
+                                        <label for="date_dni">Ingrese su Numero DNI</label>
+                                        <input type="text" class="form-control" id="date_dni" placeholder="Ingrese DNI" required>
+                                    </div>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">CERRAR FORMULARIO</button>
+                                    <button type="submit" class="btn btn-primary">ENVIAR INFORMACION</button>
+                                </div>
+                        </form>
+                </div>
+        </div>
+</div>
