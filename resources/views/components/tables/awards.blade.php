32ea61a400c71e1663a82dcf9490ec86d4dac827
MANUEL CRISTOBAL NEYRA S - SGIS-05 <MCRISTOBALN@HMPP.COM>
2025-09-01 09:46:54 -0500

    feat: table 10

--- resources/views/components/tables/awards.blade.php
+++ resources/views/components/tables/awards.blade.php
@@ -30,6 +30,86 @@
     <tr><td>9</td><td>S/  1,200.00</td><td>S/    500.00</td><td>S/  1,700.00</td></tr>
     <tr><td>10</td><td>S/    500.00</td><td>S/    300.00</td><td>S/    800.00</td></tr>
     <tr class="total"><td colspan="5"><strong>TOTAL</strong></td><td><strong>S/ 136,200.00</strong></td></tr>
+    <tr>
+      <td rowspan="5">MASTER<br>VARONES Y MUJERES (15KM)</td>
+      <td rowspan="5">De 40 a 49 años de edad</td>
+      <td>1</td>
+      <td>S/ 4,000.00</td>
+      <td>S/ 4,000.00</td>
+      <td>S/ 8,000.00</td>
+    </tr>
+    <tr><td>2</td><td>S/ 2,500.00</td><td>S/ 2,500.00</td><td>S/ 5,000.00</td></tr>
+    <tr><td>3</td><td>S/ 1,500.00</td><td>S/ 1,500.00</td><td>S/ 3,000.00</td></tr>
+    <tr><td>4</td><td>S/ 1,000.00</td><td>S/ 1,000.00</td><td>S/ 2,000.00</td></tr>
+    <tr><td>5</td><td>S/ 500.00</td><td>S/ 500.00</td><td>S/ 1,000.00</td></tr>
+    <tr class="total"><td colspan="5"><strong>TOTAL</strong></td><td><strong>S/ 19,000.00</strong></td></tr>
+     <tr>
+      <td rowspan="5">JUVENILES<br>VARONES Y MUJERES (12KM)</td>
+      <td rowspan="5">De 15 A 17 años de edad</td>
+      <td>1</td>
+      <td>S/ 2,000.00</td>
+      <td>S/ 2,000.00</td>
+      <td>S/ 4,000.00</td>
+    </tr>
+    <tr><td>2</td><td>S/ 1,500.00</td><td>S/ 1,500.00</td><td>S/ 3,000.00</td></tr>
+    <tr><td>3</td><td>S/ 1,000.00</td><td>S/ 1,000.00</td><td>S/ 2,000.00</td></tr>
+    <tr><td>4</td><td>S/ 800.00</td><td>S/ 800.00</td><td>S/ 1,600.00</td></tr>
+    <tr><td>5</td><td>S/ 500.00</td><td>S/ 500.00</td><td>S/ 1,000.00</td></tr>
+    <tr class="total"><td colspan="5"><strong>TOTAL</strong></td><td><strong>S/ 11,600.00</strong></td></tr>
+     <tr>
+      <td rowspan="5">MENORES<br>VARONES Y MUJERES (6KM)</td>
+      <td rowspan="5">De 12 A 14 años de edad</td>
+      <td>1</td>
+      <td>S/ 1,000.00</td>
+      <td>S/ 1,000.00</td>
+      <td>S/ 2,000.00</td>
+    </tr>
+    <tr><td>2</td><td>S/ 750.00</td><td>S/ 750.00</td><td>S/ 1,500.00</td></tr>
+    <tr><td>3</td><td>S/ 500.00</td><td>S/ 500.00</td><td>S/ 1,000.00</td></tr>
+    <tr><td>4</td><td>S/ 300.00</td><td>S/ 300.00</td><td>S/ 600.00</td></tr>
+    <tr><td>5</td><td>S/ 200.00</td><td>S/ 200.00</td><td>S/ 400.00</td></tr>
+    <tr class="total"><td colspan="5"><strong>TOTAL</strong></td><td><strong>S/ 5,500.00</strong></td></tr>
+     <tr>
+      <td rowspan="5">JUNIOR<br>VARONES Y MUJERES (2KM)</td>
+      <td rowspan="5">De 10 A 11 años de edad</td>
+      <td>1</td>
+      <td>S/ 500.00</td>
+      <td>S/ 500.00</td>
+      <td>S/ 1,000.00</td>
+    </tr>
+    <tr><td>2</td><td>S/ 300.00</td><td>S/ 300.00</td><td>S/ 600.00</td></tr>
+    <tr><td>3</td><td>S/ 250.00</td><td>S/ 250.00</td><td>S/ 500.00</td></tr>
+    <tr><td>4</td><td>S/ 200.00</td><td>S/ 200.00</td><td>S/ 400.00</td></tr>
+    <tr><td>5</td><td>S/ 150.00</td><td>S/ 150.00</td><td>S/ 300.00</td></tr>
+    <tr class="total"><td colspan="5"><strong>TOTAL</strong></td><td><strong>S/ 2,800.00</strong></td></tr>
+     <tr>
+      <td rowspan="5">INFANTIL<br>VARONES Y MUJERES (1KM)</td>
+      <td rowspan="5">De 7 a 9 años de edad</td>
+      <td>1</td>
+      <td>S/ 300.00</td>
+      <td>S/ 300.00</td>
+      <td>S/ 600.00</td>
+    </tr>
+    <tr><td>2</td><td>S/ 300.00</td><td>S/ 300.00</td><td>S/ 600.00</td></tr>
+    <tr><td>3</td><td>S/ 250.00</td><td>S/ 250.00</td><td>S/ 500.00</td></tr>
+    <tr><td>4</td><td>S/ 200.00</td><td>S/ 200.00</td><td>S/ 400.00</td></tr>
+    <tr><td>5</td><td>S/ 150.00</td><td>S/ 150.00</td><td>S/ 300.00</td></tr>
+    <tr class="total"><td colspan="5"><strong>TOTAL</strong></td><td><strong>S/ 2,000.00</strong></td></tr>
+    <tr>
+      <td rowspan="5">SUPER MASTER<br>VARONES Y MUJERES (5KM)</td>
+      <td rowspan="5">De 50 a MAS años de edad</td>
+      <td>1</td>
+      <td>S/ 300.00</td>
+      <td>S/ 300.00</td>
+      <td>S/ 600.00</td>
+    </tr>
+    <tr><td>2</td><td>S/ 300.00</td><td>S/ 300.00</td><td>S/ 600.00</td></tr>
+    <tr><td>3</td><td>S/ 250.00</td><td>S/ 250.00</td><td>S/ 500.00</td></tr>
+    <tr><td>4</td><td>S/ 200.00</td><td>S/ 200.00</td><td>S/ 400.00</td></tr>
+    <tr><td>5</td><td>S/ 150.00</td><td>S/ 150.00</td><td>S/ 300.00</td></tr>
+    <tr class="total"><td colspan="5"><strong>TOTAL</strong></td><td><strong>S/ 2,000.00</strong></td></tr>
+
+
 </tbody>
 
 </table>
