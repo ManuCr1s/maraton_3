0928b129b81ea0b85ab66e71a2ec6ca449f5b1a4
MANUEL CRISTOBAL NEYRA S - SGIS-05 <MCRISTOBALN@HMPP.COM>
2025-09-01 08:41:59 -0500

    feat: Add rules

--- resources/views/components/modals/bases.blade.php
+++ resources/views/components/modals/bases.blade.php
@@ -41,10 +41,161 @@ mayor nivel competitivo por parte del sector correspondiente</p>
             </ul>
             <br>
             <h6 class="text-left"><b>Artículo 3. CATEGORIAS: DISTANCIA, DENOMINACIÓN Y EDAD</b></h6>
+                <ul>
+                <div class="row">
+                    <div class="col-md-6">
+                         <li>
+                    <h6 class="text-left m-0"><b>Categoría Elite Varones desde los 18 a 39 años de edad</b></h6>
+                    <p class="text-left m-0">42,195 kilómetros de distancia</p>
+                    <p class="text-left m-0">Punto de partida: Carhuamayo</p>
+                    <p class="text-left m-0">Punto de llegada: Estadio Daniel Alcides Carrión</p>
+                  </li>
+                    <li>
+                    <h6 class="text-left m-0"><b>Categoría Master Varones y Mujeres desde los 40 a 49 años de edad</b></h6>
+                    <p class="text-left m-0">15 kilómetros de distancia</p>
+                    <p class="text-left m-0">Punto de partida: Colquijirca</p>
+                    <p class="text-left m-0">Punto de llegada: Estadio Daniel Alcides Carrión</p>
+                  </li>
+                    <li>
+                    <h6 class="text-left m-0"><b>Categoría Menores Varones y Mujeres desde los 12 a 14 años de edad</b></h6>
+                    <p class="text-left m-0">06 kilómetros de distancia</p>
+                    <p class="text-left m-0">Punto de partida: Cruce entre Huánuco y Pasco</p>
+                    <p class="text-left m-0">Punto de llegada: Estadio Daniel Alcides Carrión</p>
+                  </li>
+                    <li>
+                    <h6 class="text-left m-0"><b>Categoría Junior Varones y Mujeres desde los10 a 11 años de edad</b></h6>
+                    <p class="text-left m-0">2 kilómetros de distancia</p>
+                    <p class="text-left m-0">Punto de partida: Cruce entre Moquegua y Bolognesi</p>
+                    <p class="text-left m-0">Punto de llegada: Estadio Daniel Alcides Carrión</p>
+                  </li>
+                    <li>
+                    <h6 class="text-left m-0"><b>Categoría Habilidades Especiales (Sordo y Mudo)</b></h6>
+                    <p class="text-left m-0">5 kilómetros de distancia</p>
+                    <p class="text-left m-0">Punto de partida: Arco de Bienvenida a Pasco</p>
+                    <p class="text-left m-0">Punto de llegada: Estadio Daniel Alcides Carrión</p>
+                  </li>
+         
+                    </div>
 
-           
-            
+                    <div class="col-md-6">
+                  <li>
+                    <h6 class="text-left m-0"><b>Categoría Elite Damas desde los 18 a 39 años de edad</b></h6>
+                    <p class="text-left m-0">21 kilómetros de distancia.</p>
+                    <p class="text-left m-0">Punto de partida: Villa de Pasco</p>
+                    <p class="text-left m-0">Punto de llegada: Estadio Daniel Alcides Carrión</p>
+                  </li>
+                    <li>
+                    <h6 class="text-left m-0"><b>Categoría Juveniles Varones y Mujeres de los 15 a 17 años de edad</b></h6>
+                    <p class="text-left m-0">12 kilómetros de distancia</p>
+                    <p class="text-left m-0">Punto de partida: Grifo la Victoria</p>
+                    <p class="text-left m-0">Punto de llegada: Estadio Daniel Alcides Carrión</p>
+                  </li>
+                    <li>
+                    <h6 class="text-left m-0"><b>Categoría Súper Master Varones y Mujeres desde los 50 años de edad a ma</b></h6>
+                    <p class="text-left m-0">5 kilómetros de distancia</p>
+                    <p class="text-left m-0">Punto de partida: Arco de Bienvenida de Pasco</p>
+                    <p class="text-left m-0">Punto de llegada: Estadio Daniel Alcides Carrión</p>
+                  </li>
+                    <li>
+                    <h6 class="text-left m-0"><b>Categoría Infantil Varones y Mujeres desde los 7 a 9 años de edad</b></h6>
+                    <p class="text-left m-0">1 kilómetros de distancia</p>
+                    <p class="text-left m-0">Municipalidad Provincial de Pasco</p>
+                    <p class="text-left m-0">Punto de llegada: Estadio Daniel Alcides Carrión</p>
+                  </li>
+                    <li>
+                    <h6 class="text-left m-0"><b>Categoría Habilidades Especiales (con ceguera)</b></h6>
+                    <p class="text-left m-0">2 kilómetros de distancia</p>
+                    <p class="text-left m-0">Punto de partida: Cruce entre Moquegua y Bolognesi</p>
+                    <p class="text-left m-0">Punto de llegada: Estadio Daniel Alcides Carrión</p>
+                  </li>
+                    </div>
+                </div>
+                </ul>
+                          <br>
+              <h6 class="text-left"><b>Artículo 4. DE LAS INSCRIPCIONES</b></h6>
+              <p class="text-left m-0">Las inscripciones se realizarán a través de página web. http://maraton.munipasco.gob.pe de
+                manera virtual y presencial en la oficina de Sub Gerencia de Educación, Cultura, Deportes y Juventudes de la
+                Municipalidad Provincial de Pasco y la Sub Gerencia de servicios públicos y sociales de la Municipalidad Distrital
+                de Carhuamayo de en horario de oficina.</p>
+              <ul>
+                <li>
+                  <p class="text-left m-0">El periodo de inscripciones se inicia desde el 2 de setiembre hasta el jueves 23 de octubre hasta las 17.00 Horas</p>
+                </li>
+                <li>
+                  <p class="text-left m-0">Solo se aceptará 250 participantes por categoría entre varones y mujeres</p>
+                </li>
+                <li>
+                  <p class="text-left m-0">Copia de DNI y/o pasaporte de extranjería vigente</p>
+                </li>
+                <li>
+                  <p class="text-left m-0">Carnet de CONADIS color amarillo (solo para las personas discapacitadas)</p>
+                </li>
+                <li>
+                  <p class="text-left m-0">Examen médico de tener buena salud</p>
+                </li>
+                <li>
+                  <p class="text-left m-0">Los participantes deberán firmar en el momento de la inscripción el compromiso (Adjunto) donde libera a
+los organizadores de toda responsabilidad y reclamo alguno por parte del atleta o sus familiares</p>
+                </li>
+                <li>
+                  <p class="text-left m-0">Es requisito indispensable contar con la revisión médica (Constancia Medica), y la autorización de sus
+padres y/o apoderados para los menores de edad (Formato 1 Adjunto)</p>
+                </li>
+                <li>
+                  <p class="text-left m-0">Costo de inscripción: La inscripción es totalmente gratis</p>
+                </li>
+                <li>
+                  <p class="text-left m-0">Para poder recoger su kit de competidor, deberán presentar la confirmación de inscripción (código) más
+su D.N.I. o Carnet de extranjería.</p>
+                </li>
+              </ul>
+               <br>
+              <h6 class="text-left"><b>Artículo 5. DE LOS SERVICIOS</b></h6>
+              <ul>
+                <li>
+                  <p class="text-left m-0">Los organizadores entregaran convenientemente polos Atléticos para los competidores de todas las
+Categorías, con su respectiva numeración el día viernes 24 y sabado 25 de octubre a partir de 10:00
+am hasta las 04:00 pm.</p>
+                </li>
+                <li>
+                  <p class="text-left m-0">Asistencia médica antes, durante y después de la competencia</p>
+                </li>
+                <li>
+                  <p class="text-left m-0">Servicio de seguridad y control (Serenazgo, Bomberos, Policía Nacional, entre otros)</p>
+                </li>
+                <li>
+                  <p class="text-left m-0">No se hará entrega por ningún motivo el kit al participante, el mismo día de la competencia.</p>
+                </li>
+              </ul>
             <br>
+              <h6 class="text-left"><b>Artículo 6. DE LA COMPETENCIA</b></h6>
+              <ul>
+                <li>
+                  <p class="text-left m-0">La prueba se cumplirá el día domingo 26 de octubre, según el horario establecido por todas las categorías,
+siendo la hora de salida a los puntos de partida a las 7:00 am. La concentración se dará a partir de las
+06:00 am, en la plazuela municipal de la HMPP</p>
+                </li>
+                <li>
+                  <p class="text-left m-0">Los atletas deberán presentarse media hora antes de la partida con indumentaria deportiva (Polo de atleta
+otorgada por la comisión, Short y zapatillas que se adecuarán ellos mismos)</p>
+                </li>
+                <li>
+                  <p class="text-left m-0">Los competidores y/o atletas deben respetar la ruta según categoría.</p>
+                </li>
+                <li>
+                  <p class="text-left m-0">Atleta que no lleve el polo de inscripción y no realice el recorrido correspondiente será descalificado sin
+lugar a reclamo.</p>
+                </li>
+                <li>
+                  <p class="text-left m-0">El punto de partida de los maratonistas en sus diferentes categorías será a las 09:00 am</p>
+                </li>
+                <li>
+                  <p class="text-left m-0">Cada corredor recibirá una bolsa para guardar su ropa en la que anotará su número de competencia y
+deberá entregarlo al carro oficial encargado de la indumentaria, la que se responsabilizará de su custodia
+y entrega al término de la prueba en el Estadio Daniel Alcides Carrión donde será el punto de llagada de
+todos los atletas</p>
+                </li>
+              </ul>
       </div>
       <div class="modal-footer">
 
