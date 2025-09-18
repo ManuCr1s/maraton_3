02171eaa51ae09b992bd9ae7c3aab10b8ff6cda4
MANUEL CRISTOBAL NEYRA S - SGIS-05 <MCRISTOBALN@HMPP.COM>
2025-09-18 17:22:40 -0500

    fic: Table awards

--- resources/views/components/tables/awards.blade.php
+++ resources/views/components/tables/awards.blade.php
@@ -20,15 +20,15 @@
       <td>S/ 12,500.00</td>
       <td>S/ 37,500.00</td>
     </tr>
-    <tr><td>2</td><td>S/ 15,000.00</td><td>S/ 10,000.00</td><td>S/ 25,000.00</td></tr>
-    <tr><td>3</td><td>S/ 15,000.00</td><td>S/  9,000.00</td><td>S/ 24,000.00</td></tr>
+    <tr><td>2</td><td>S/ 21,000.00</td><td>S/ 10,000.00</td><td>S/ 31,000.00</td></tr>
+    <tr><td>3</td><td>S/ 16,000.00</td><td>S/  8,000.00</td><td>S/ 24,000.00</td></tr>
     <tr><td>4</td><td>S/ 13,000.00</td><td>S/  5,000.00</td><td>S/ 18,000.00</td></tr>
-    <tr><td>5</td><td>S/  8,000.00</td><td>S/  4,000.00</td><td>S/ 12,000.00</td></tr>
-    <tr><td>6</td><td>S/  5,000.00</td><td>S/  1,000.00</td><td>S/  6,000.00</td></tr>
-    <tr><td>7</td><td>S/  3,000.00</td><td>S/    500.00</td><td>S/  3,500.00</td></tr>
-    <tr><td>8</td><td>S/  2,000.00</td><td>S/    500.00</td><td>S/  2,500.00</td></tr>
-    <tr><td>9</td><td>S/  1,200.00</td><td>S/    500.00</td><td>S/  1,700.00</td></tr>
-    <tr><td>10</td><td>S/    500.00</td><td>S/    300.00</td><td>S/    800.00</td></tr>
+    <tr><td>5</td><td>S/  9,000.00</td><td>S/  3,000.00</td><td>S/ 12,000.00</td></tr>
+    <tr><td>6</td><td>S/  4,000.00</td><td>S/  2,000.00</td><td>S/  6,000.00</td></tr>
+    <tr><td>7</td><td>S/  3,000.00</td><td>S/  1,000.00</td><td>S/  4,000.00</td></tr>
+    <tr><td>8</td><td>S/  1,000.00</td><td>S/    700.00</td><td>S/  1,700.00</td></tr>
+    <tr><td>9</td><td>S/  800.00</td><td>S/    400.00</td><td>S/  1,200.00</td></tr>
+    <tr><td>10</td><td>S/    500.00</td><td>S/    300.00</td><td>S/   800.00</td></tr>
     <tr class="total"><td colspan="5"><strong>TOTAL</strong></td><td><strong>S/ 136,200.00</strong></td></tr>
     <tr>
       <td rowspan="5">MASTER<br>VARONES Y MUJERES (15KM)</td>
