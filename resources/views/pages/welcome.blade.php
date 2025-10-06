2e756ad15d343426b008045e36b42ab79834bb32
MANUEL CRISTOBAL NEYRA S - SGIS-05 <MCRISTOBALN@HMPP.COM>
2025-10-06 08:34:08 -0500

    feat: Add Imgae

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -129,9 +129,9 @@
           </div>
           <div class="row pl-5">
               <div class="col-md-3"><img src="{{asset('assets/img/logo/alquitara.png')}}" alt="ALQUITARA" class="w-50 my-5 mx-2"></div>
-              <div class="col-md-3"><img src="{{asset('assets/img/logo/salka.png')}}" alt="ALQUITARA" class="w-50 my-5 mx-2"></div>
-              <div class="col-md-3"></div>
-              <div class="col-md-3"></div>
+              <div class="col-md-3"><img src="{{asset('assets/img/logo/salka.png')}}" alt="SALKA" class="w-50 my-5 mx-2"></div>
+              <div class="col-md-3"><img src="{{asset('assets/img/logo/ipd.png')}}" alt="IPD" class="w-50 my-5 mx-2"></div>
+              <div class="col-md-3"><img src="{{asset('assets/img/logo/alquitaravip.png')}}" alt="ALQUITARA VIP" class="w-50 my-5 mx-2"></div>
           </div>
       </div>
 
